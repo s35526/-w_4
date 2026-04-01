670aac8f483d1cabb585e6287dcc9bfa6ec66617
s35526 <s35526@pjwstk.edu.pl>
2026-04-01 09:10:32 +0200

    Początki tworzenia  klasy Polisa

---
+++ untitled/src/Polisa.java
@@ -0,0 +1,14 @@
+public class Polisa {
+
+    private String numerPolisy;
+    private String klient;
+    private double skladkaBazowa;
+    private int poziomRyzyka;
+    private double wartoscPojazdu;
+    private boolean czyMaAlarm;
+    private boolean czyBezszkodowyKlient;
+    private static int liczbaUtworzonychPolis;
+    private static final double OPLATA_ADMINISTRACYJNA
+
+    public Polisa(String numerPolisy,String klient,double skladkaBazowa,int poziomRyzyka,double wartoscPojazdu,boolean czyMaAlarm,boolean czyBezszkodowyKlient)
+}
