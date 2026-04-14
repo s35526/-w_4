80dada6b98697f56a4d64d2599cf56ac2fb955b6
s35526 <s35526@pjwstk.edu.pl>
2026-04-14 11:19:05 +0200

    Wypełnienie i dodanie metod w klasie BiuroUbezpieczen

--- Kod/src/BiuroUbezpieczen.java
+++ Kod/src/BiuroUbezpieczen.java
@@ -1,2 +1,59 @@
+import java.util.ArrayList;
+
 public class BiuroUbezpieczen {
+
+    private String nazwa;
+    private ArrayList<Polisa> polisy;
+
+    public BiuroUbezpieczen(String nazwa) {
+        this.nazwa = nazwa;
+        this.polisy = new ArrayList<>();
+    }
+
+    public void dodajPolisa(Polisa polisa) {
+        polisy.add(polisa);
+    }
+
+    public void wypiszRaport() {
+        System.out.println("=== RAPORT: " + nazwa + "===");
+        for (Polisa p : polisy) {
+            System.out.println(p);
+        }
+        System.out.println("Laczna skladka: " + policzLacznaSkladke());
+    }
+
+    public double policzLacznaSkladke() {
+        double suma = 0;
+        for (Polisa p : polisy) {
+            suma += p.obliczSkladkeKoncowa();
+        }
+        return Math.round(suma * 100.0) / 100.0;
+    }
+
+    public int policzPolisyWysokiegoRyzyka() {
+        int licznik = 0;
+        for (Polisa p : polisy) {
+            if (p.getPoziomRyzyka() >= 4) {
+                licznik++;
+            }
+            return licznik;
+        }
+    }
+
+    public Polisa znajdzPoNumerze(String numerPolisy) {
+        for (Polisa p : polisy) {
+            if (p.getNumerPolisy().equals(numerPolisy)) return p;
+        }
+        return null;
+    }
+
+    public void wypiszTanszeNiz(double prog) {
+        System.out.println("Polisy ponizej" + prog + "zl:");
+        for (Polisa p : polisy) {
+            if (p.obliczSkladkeKoncowa() < prog) {
+                System.out.println(" " + p);
+            }
+        }
+    }
 }
+
--- Kod/src/Main.java
+++ Kod/src/Main.java
@@ -5,3 +5,13 @@ public class Main {
         Polisa p1 = new Polisa("CAR-101", "Anna Nowak", 900.0, 3, 72000.0, true, true);
         Polisa p2 = new Polisa("CAR-102", "Piotr Lis", 840.0, 4, 54000.0, false, false);
         Polisa p3 = new Polisa("CAR-103", "Karolina Maj", 780.0, 2, 46000.0, true, false);
+
+    biuro.dodajPolisa(p1);
+    biuro.dodajPolisa(p2);
+    biuro.dodajPolisa(p3);
+
+    biuro.wypiszRaport();
+
+    System.out.println("Laczna składka: " + biuro.policzLacznaSkladke());
+    }
+}
